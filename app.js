07cc0d2adf3ef0329d6703ee6bcfeacc996613ee
Caris <cwill1013@gmail.com>
2024-02-28 17:05:01 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -8,76 +8,84 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
-let a = 1;
-while (a < 100) {
-    if (a % 2 !== 0) {
-        console.log(a);
-    }
-}
+
 // Exercise 2 Section
 
-for (let i = 1; i < 100; i++) {
-    console.log(i);
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FIZZBUZZ");
-    } else {
-        if(i % 3 == 0) {
-            console.log("FIZZ")
-        }
-        if (i % 5 == 0) {
-            console.log("BUZZ");
-        }
-    }
+for (let i = 1; i<= 100; i++)
+{
+    let output = "";
+
+if (i % 3 == 0)
+{
+    output += "FIZZ";
 }
 
+if (i % 5 == 0)
+{
+    output += "BUZZ";
+}
+console.log(`${i} ${output}`);
+}
 
 // Exercise 3 
 
+let c = 1;
+
+while (c < 100)
+{
+    if (c % 2 !== 1)
+    {
+        console.log(c)
+    }
+    c++;
+}
+
+let a =1;
+
+do 
+{
+    if (a % 2 !== 0 )
+    {
+        console.log(a);
+    }
+    a++;
+} while(a < 100);
+
+
 let d = 1;
 
-do {
-    if (d % 2 !==0) {
-        console.log(d);
-    }
-    d++;
-} while (d < 100);
-
-let c = 1;
-while(c <= 100) {
+do 
+{
     let output = "";
-    if (c % 3 == 0) {
+
+    if (d % 3 == 0)
+    {
         output += "FIZZ";
     }
-    if (c % 5 == 0) {
-        output += "BUZZ"
+    if (d %5 == 0)
+    {
+        output += "BUZZ";
     }
-    console.log(`${i} ${output}`);
-    i++;
-}
+    console.log(`${d} ${output}`);
+
+    d++;
+} while (d <= 100);
+
 
 // Exercise 4
-console.log("EXERCISE 4: \n==========\n"); 
-let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
-     
-let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
-console.log("Random value:")
-console.log(value);
-console.log("end point");
-console.log(n);
-console.log("Checking to see if we have the value....")
 
-let isFound = false;
-for( let i = 1; i <= n; i++) {
-    if(i == value) {
-        console.log("Found Value!!");
-        isFound = true;
+let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+
+for (let i = 1; i <= n; i++)
+{
+    if (i == numberToFind)
+    {
+        console.log(`Found ${numberToFind}!`);
         break;
     }
-    else {
-        console.log("Did not find value");
-        isFound = false;
-    }
 }
-if(isFound == false) {
-    console.log("Did not find value")
+if (i ==n){
+console.log(`Did not find ${numberToFind} within 1 - ${n}..`);
 }
